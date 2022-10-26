5f9648a1fd3397f31c178cea081359b5a3b27d0e
Chris Artates <ch1artates@gmail.com>
2022-10-26 08:44:36 +0800

    hid api keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState();
+  const [city, setCity] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_KEY}`;
+
+  const getWeatherData = async (e) => {
+    if (e.key === "Enter") {
+      axios.get(url).then((res) => {
+        setData(res.data);
+      });
+      setCity("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="search">
+        <input
+          type="text"
+          value={city}
+          onKeyPress={getWeatherData}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+      </div>
+      Temp:{data && data.main.temp}°C
+    </div>
+  );
 }
 
 export default App;
